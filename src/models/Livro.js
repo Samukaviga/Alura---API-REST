a5bfaf1cf147ba9c0ccc73bd02d528640fc4a7e9
Samuel <samuelgomes2021@gmail.com>
2023-04-22 16:03:32 -0300

    Validação Mongoose. String e Int (enum, min, max)

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,11 +2,31 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: {type: String},
-    titulo: {type: String, required: [true, "O nome do livro é obrigatório."]},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O autor(a) é obrigatório."]},
-    editora: {type: String, required: [true, "O nome da editora é obrigatório."]},
-    numeroPaginas: {type: Number}
+    id: {
+      type: String
+    },
+    titulo: {
+      type: String, 
+      required: [true, "O nome do livro é obrigatório."]
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "autores", 
+      required: [true, "O autor(a) é obrigatório."]
+    },
+    editora: {
+      type: String, 
+      required: [true, "O nome da editora é obrigatório."],
+      enum: {   
+        values: ["Suma de letras", "ingles"],  //Editoras obrigatórias e unicas para STRINGs
+        message: "A editora {VALUE} não é um nome válido."
+      }
+    },
+    numeroPaginas: {
+      type: Number, 
+      min: [10, "O numero de paginas deve estar entre 10 e 5000."], 
+      max: [5000, "O numero de paginas deve estar entre 10 e 5000"]
+    } 
   }
 );
 
